91413d755578ea681bdd5ff2def1e5544523fefa
T.leo <tambalyo@gmail.com>
2021-11-18 15:03:08 +0100

    Add project files.

---
+++ MobileSanteApp.DataAccess/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace MobileSanteApp.DataAccess
+{
+    public class Class1
+    {
+    }
+}
---
+++ MobileSanteApp.Models/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace MobileSanteApp.Models
+{
+    public class Class1
+    {
+    }
+}
